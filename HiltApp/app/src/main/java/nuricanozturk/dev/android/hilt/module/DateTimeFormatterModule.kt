357fd6e697960256e48afc2493ce742eb2bdf087
nuricanozturk01 <18070006034@stu.yasar.edu.tr>
2023-10-01 00:29:11 +0300

    updated

--- HiltApp/app/src/main/java/nuricanozturk/dev/android/hilt/module/DateTimeFormatterModule.kt
+++ HiltApp/app/src/main/java/nuricanozturk/dev/android/hilt/module/DateTimeFormatterModule.kt
@@ -1,15 +1,24 @@
 package nuricanozturk.dev.android.hilt.module
 
+import android.content.Context
+import android.widget.Toast
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import java.time.format.DateTimeFormatter
+import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 object DateTimeFormatterModule
 {
     @Provides
-    fun createDateTimeFormatter() : DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy kk:mm:ss")
+    @Singleton
+    fun createDateTimeFormatter(@ApplicationContext context : Context) : DateTimeFormatter
+    {
+        Toast.makeText(context, "date-time-provider-singletor", Toast.LENGTH_LONG).show()
+        return DateTimeFormatter.ofPattern("dd/MM/yyyy kk:mm:ss")
+    }
 }
--- HiltApp/app/src/main/java/nuricanozturk/dev/android/hilt/module/DateTimeModule.kt
+++ HiltApp/app/src/main/java/nuricanozturk/dev/android/hilt/module/DateTimeModule.kt
@@ -1,16 +1,24 @@
 package nuricanozturk.dev.android.hilt.module
 
+import android.content.Context
+import android.widget.Toast
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import java.time.LocalDateTime
 
 @Module
 @InstallIn(ActivityComponent::class) // scope boyu
 object DateTimeModule
 {
+
     @Provides
-    fun createLocalDateTime() : LocalDateTime = LocalDateTime.now();
+    fun createLocalDateTime(@ApplicationContext context : Context) : LocalDateTime
+    {
+        Toast.makeText(context, "date-time", Toast.LENGTH_LONG).show()
+        return LocalDateTime.now();
+    }
 
 }
