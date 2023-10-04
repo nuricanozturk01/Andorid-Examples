a250da25d0c03bbf9d24472311823a943c43a652
Nuri Can ÖZTÜRK <62218588+nuricanozturk01@users.noreply.github.com>
2023-10-04 23:06:41 +0300

    update

--- Libs/CalculatorLib/CalculatorLib/src/main/java/nuricanozturk/dev/calculatorlib/IBinaryOperator.kt
+++
@@ -1,6 +0,0 @@
-package nuricanozturk.dev.calculatorlib
-
-interface IBinaryOperator<T> {
-    fun applyAsInt(a: T, b: T) : T
-    fun isValid(op: Char) : Boolean
-}
--- Libs/CalculatorLib/CalculatorLib/src/main/java/nuricanozturk/dev/calculatorlib/di/module/annotation/IntMultiplyOperationInterceptor.kt
+++
@@ -1,7 +0,0 @@
-package nuricanozturk.dev.calculatorlib.di.module.annotation
-
-import javax.inject.Qualifier
-
-@Qualifier
-@Retention(AnnotationRetention.BINARY)
-annotation class IntMultiplyOperationInterceptor
