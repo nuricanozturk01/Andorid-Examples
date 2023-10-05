df7a5b5a6526d1d82c71fe0ffedb385ddf3576f7
nuricanozturk01 <18070006034@stu.yasar.edu.tr>
2023-10-05 22:17:47 +0300

    update

--- Projects/MultipleActivity/app/src/main/java/nuricanozturk/dev/android/multipleactivity/RegisterActivity.kt
+++ Projects/MultipleActivity/app/src/main/java/nuricanozturk/dev/android/multipleactivity/RegisterActivity.kt
@@ -8,25 +8,57 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import nuricanozturk.dev.android.multipleactivity.databinding.ActivityRegisterBinding
+import nuricanozturk.dev.android.multipleactivity.viewmodel.RegisterActivityViewModel
 import nuricanozturk.dev.android.multipleactivity.viewmodel.RegisterInfo
+import java.time.LocalDate
 
 class RegisterActivity : AppCompatActivity()
 {
     private lateinit var mBinding: ActivityRegisterBinding
+    private lateinit var mMonths : Array<String>
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun initViewModels()
     {
         mBinding.registerInfo = RegisterInfo()
+        mBinding.viewModel = RegisterActivityViewModel(this)
 
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun initBinding()
     {
+        mMonths = resources.getStringArray(R.array.months)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_register)
         initViewModels()
         initEducationAdapter()
+        initBirthDateAdapters()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getDaysByMonthAndYear(monthPos: Int, yearPos: Int) : List<Int>
+    {
+        val max = LocalDate.of(yearPos, monthPos + 1, 1).minusDays(1).dayOfMonth
+
+        return (1..max).toList()
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun initBirthDateTexts(today: LocalDate)
+    {
+        mBinding.daySelectedPos = today.dayOfMonth - 1
+        mBinding.monthSelectedPos = today.monthValue - 1
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun initBirthDateAdapters()
+    {
+        val today = LocalDate.now()
+        val years = (today.year - 100..today.minusYears(19).year).toList()
+        mBinding.dayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, getDaysByMonthAndYear(today.monthValue, today.year))
+        mBinding.monthAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, mMonths)
+        mBinding.yearAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, years)
+        mBinding.yearSelectedPos = mBinding.yearAdapter!!.count - 1
+        initBirthDateTexts(today)
     }
 
     private fun initEducationAdapter() {
@@ -48,7 +80,9 @@ class RegisterActivity : AppCompatActivity()
 
     fun registerButtonClicked()
     {
-        TODO("Not implemented yet!...")
+
+        Toast.makeText(this, "Hi! " + mBinding.registerInfo!!.firstName, Toast.LENGTH_LONG)
+            .show()
     }
 
     fun clearButtonClicked()
@@ -65,6 +99,21 @@ class RegisterActivity : AppCompatActivity()
     {
         val selected = mBinding.educationAdapter!!.getItem(pos)
 
-        Toast.makeText(this, selected, Toast.LENGTH_LONG).show()
+        Toast.makeText(this@RegisterActivity, selected, Toast.LENGTH_LONG).show()
+    }
+
+    fun handleChangeDay(pos : Int)
+    {
+
+    }
+
+    fun handleChangeMonth(pos : Int)
+    {
+
+    }
+
+    fun handleChangeYear(pos : Int)
+    {
+
     }
 }
--- Projects/MultipleActivity/app/src/main/java/nuricanozturk/dev/android/multipleactivity/viewmodel/RegisterActivityViewModel.kt
+++ Projects/MultipleActivity/app/src/main/java/nuricanozturk/dev/android/multipleactivity/viewmodel/RegisterActivityViewModel.kt
@@ -4,12 +4,14 @@ import nuricanozturk.dev.android.multipleactivity.RegisterActivity
 import java.lang.ref.WeakReference
 
 
-class RegisterActivityViewModel(activity : RegisterActivity,
-                                var username : String = "",
-                                var password : String = "")
+class RegisterActivityViewModel(activity : RegisterActivity)
 {
-
     private val mWeakReference = WeakReference(activity)
+
     fun handleRegisterButton() = mWeakReference.get()?.registerButtonClicked()
-    fun handleChangeEducation(pos: Int) = mWeakReference.get()?.handleChangeEducation(pos)
+
+    fun handleChangeEducation(pos : Int) = mWeakReference.get()?.handleChangeEducation(pos)
+    fun handleChangeDay(pos : Int) = mWeakReference.get()?.handleChangeDay(pos)
+    fun handleChangeMonth(pos : Int) = mWeakReference.get()?.handleChangeMonth(pos)
+    fun handleChangeYear(pos : Int) = mWeakReference.get()?.handleChangeYear(pos)
 }
