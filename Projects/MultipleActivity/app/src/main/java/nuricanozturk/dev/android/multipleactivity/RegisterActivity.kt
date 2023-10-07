f4ac2285415ef602e532143f2c475bf2c164330a
nuricanozturk01 <18070006034@stu.yasar.edu.tr>
2023-10-07 19:53:48 +0300

    repository

--- Projects/MultipleActivity/app/src/main/java/nuricanozturk/dev/android/multipleactivity/RegisterActivity.kt
+++ Projects/MultipleActivity/app/src/main/java/nuricanozturk/dev/android/multipleactivity/RegisterActivity.kt
@@ -14,7 +14,7 @@ import java.time.LocalDate
 
 class RegisterActivity : AppCompatActivity()
 {
-    private lateinit var mBinding: ActivityRegisterBinding
+    private lateinit var mBinding : ActivityRegisterBinding
     private lateinit var mMonths : Array<String>
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -36,14 +36,15 @@ class RegisterActivity : AppCompatActivity()
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun getDaysByMonthAndYear(monthPos: Int, yearPos: Int) : List<Int>
+    private fun getDaysByMonthAndYear(monthPos : Int, yearPos : Int) : List<Int>
     {
         val max = LocalDate.of(yearPos, monthPos + 1, 1).minusDays(1).dayOfMonth
-
+        Toast.makeText(this, "max is: $max", Toast.LENGTH_LONG).show()
         return (1..max).toList()
     }
+
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun initBirthDateTexts(today: LocalDate)
+    private fun initBirthDateTexts(today : LocalDate)
     {
         mBinding.daySelectedPos = today.dayOfMonth - 1
         mBinding.monthSelectedPos = today.monthValue - 1
@@ -53,17 +54,27 @@ class RegisterActivity : AppCompatActivity()
     private fun initBirthDateAdapters()
     {
         val today = LocalDate.now()
-        val years = (today.year - 100..today.minusYears(19).year).toList()
-        mBinding.dayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, getDaysByMonthAndYear(today.monthValue, today.year))
+        val years = (today.year - 100..today.year).toList()
+        mBinding.dayAdapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, getDaysByMonthAndYear(today.monthValue, today.year))
         mBinding.monthAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, mMonths)
         mBinding.yearAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, years)
         mBinding.yearSelectedPos = mBinding.yearAdapter!!.count - 1
         initBirthDateTexts(today)
     }
 
-    private fun initEducationAdapter() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun birthDateMonthSpinnerItemSelected(pos : Int)
+    {
+        mBinding.dayAdapter?.clear()
+        mBinding.dayAdapter?.addAll(getDaysByMonthAndYear(pos + 1, mBinding.yearSelectedPos))
+    }
+
+    private fun initEducationAdapter()
+    {
         val info = resources.getStringArray(R.array.spinner_education_info)
-        mBinding.educationAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, info)
+        mBinding.educationAdapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, info)
         mBinding.educationSelectedPost = 2
     }
 
@@ -72,7 +83,7 @@ class RegisterActivity : AppCompatActivity()
     private fun initialize() = initBinding()
 
     @RequiresApi(Build.VERSION_CODES.O)
-    override fun onCreate(savedInstanceState: Bundle?)
+    override fun onCreate(savedInstanceState : Bundle?)
     {
         super.onCreate(savedInstanceState)
         initialize()
@@ -81,8 +92,7 @@ class RegisterActivity : AppCompatActivity()
     fun registerButtonClicked()
     {
 
-        Toast.makeText(this, "Hi! " + mBinding.registerInfo!!.firstName, Toast.LENGTH_LONG)
-            .show()
+        Toast.makeText(this, "Hi! " + mBinding.registerInfo!!.firstName, Toast.LENGTH_LONG).show()
     }
 
     fun clearButtonClicked()
@@ -95,7 +105,7 @@ class RegisterActivity : AppCompatActivity()
         TODO("Not implemented yet!...")
     }
 
-    fun handleChangeEducation(pos: Int)
+    fun handleChangeEducation(pos : Int)
     {
         val selected = mBinding.educationAdapter!!.getItem(pos)
 
--- Projects/MultipleActivity/app/src/main/java/nuricanozturk/dev/android/multipleactivity/viewmodel/RegisterActivityViewModel.kt
+++ Projects/MultipleActivity/app/src/main/java/nuricanozturk/dev/android/multipleactivity/viewmodel/RegisterActivityViewModel.kt
@@ -1,5 +1,7 @@
 package nuricanozturk.dev.android.multipleactivity.viewmodel
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import nuricanozturk.dev.android.multipleactivity.RegisterActivity
 import java.lang.ref.WeakReference
 
@@ -12,6 +14,7 @@ class RegisterActivityViewModel(activity : RegisterActivity)
 
     fun handleChangeEducation(pos : Int) = mWeakReference.get()?.handleChangeEducation(pos)
     fun handleChangeDay(pos : Int) = mWeakReference.get()?.handleChangeDay(pos)
-    fun handleChangeMonth(pos : Int) = mWeakReference.get()?.handleChangeMonth(pos)
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun handleChangeMonth(pos : Int) = mWeakReference.get()?.birthDateMonthSpinnerItemSelected(pos)
     fun handleChangeYear(pos : Int) = mWeakReference.get()?.handleChangeYear(pos)
 }
